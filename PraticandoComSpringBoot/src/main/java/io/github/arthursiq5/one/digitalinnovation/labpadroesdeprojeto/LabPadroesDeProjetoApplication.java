212abf45733f21d68dc8a35bdff5a2f58f4a5241
arthursiq5 <arthur.siq5@gmail.com>
2023-04-07 19:12:00 -0300

    Iniciado projeto com Spring Boot

---
+++ PraticandoComSpringBoot/src/main/java/io/github/arthursiq5/one/digitalinnovation/labpadroesdeprojeto/LabPadroesDeProjetoApplication.java
@@ -0,0 +1,13 @@
+package io.github.arthursiq5.one.digitalinnovation.labpadroesdeprojeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabPadroesDeProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabPadroesDeProjetoApplication.class, args);
+	}
+
+}
