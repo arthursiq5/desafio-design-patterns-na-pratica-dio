71037313f00ca41fefdb1e7b06dc8fc644e489bf
arthursiq5 <arthur.siq5@gmail.com>
2023-04-07 17:49:59 -0300

    Criadas classes de comportamento restantes

---
+++ PraticandoComJavaPuro/src/io/github/arthursiq5/dio/desafio/src/strategy/ComportamentoAgressivo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package io.github.arthursiq5.dio.desafio.src.strategy;
+
+/**
+ *
+ * @author arthur
+ */
+public class ComportamentoAgressivo implements Comportamento {
+
+    @Override
+    public void mover() {
+        System.out.println("Movendo-se de forma agressiva");
+    }
+    
+}
