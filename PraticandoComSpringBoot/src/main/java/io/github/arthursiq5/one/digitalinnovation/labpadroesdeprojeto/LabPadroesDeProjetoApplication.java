3fe7ada8f46351c51e2a2187520ea6af0e4f3923
arthursiq5 <arthur.siq5@gmail.com>
2023-04-07 19:29:36 -0300

    Iniciado Feign

--- PraticandoComSpringBoot/src/main/java/io/github/arthursiq5/one/digitalinnovation/labpadroesdeprojeto/LabPadroesDeProjetoApplication.java
+++ PraticandoComSpringBoot/src/main/java/io/github/arthursiq5/one/digitalinnovation/labpadroesdeprojeto/LabPadroesDeProjetoApplication.java
@@ -2,7 +2,13 @@ package io.github.arthursiq5.one.digitalinnovation.labpadroesdeprojeto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * Projeto Spring Boot
+ * @author arthur
+ */
+@EnableFeignClients
 @SpringBootApplication
 public class LabPadroesDeProjetoApplication {
 
