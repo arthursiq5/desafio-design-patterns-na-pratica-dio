9f719ae258ec99a15732f110e5ec8085f3d035a6
arthursiq5 <arthur.siq5@gmail.com>
2023-04-07 17:54:34 -0300

    Adicionadas dependências

---
+++ PraticandoComJavaPuro/src/io/github/arthursiq5/dio/desafio/src/facade/subprojeto/CepApi.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package io.github.arthursiq5.dio.desafio.src.facade.subprojeto;
+
+/**
+ *
+ * @author arthur
+ */
+public class CepApi {
+    private static CepApi instancia = new CepApi();
+
+    private CepApi() {
+        super();
+    }
+
+    public static CepApi getInstancia() {
+        return instancia;
+    }
+
+    public String recuperarCidade(String cep) {
+        return "Araraquara";
+    }
+
+    public String recuperarEstado(String cep) {
+        return "SP";
+    }
+}
