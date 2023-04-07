9c4a333885407c6032ef7ff587f1080d3b3f7710
arthursiq5 <arthur.siq5@gmail.com>
2023-04-07 17:46:31 -0300

    Adicionada interface Iniciada implementação de strategy

---
+++ PraticandoComJavaPuro/src/io/github/arthursiq5/dio/desafio/src/strategy/Comportamento.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package io.github.arthursiq5.dio.desafio.src.strategy;
+
+/**
+ *
+ * @author arthur
+ */
+public interface Comportamento {
+    void mover();
+}
