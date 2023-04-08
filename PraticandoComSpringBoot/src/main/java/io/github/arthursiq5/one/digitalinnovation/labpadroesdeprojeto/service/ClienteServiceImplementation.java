10eec31de26ac619229dd08405755bc2e05372e1
arthursiq5 <arthur.siq5@gmail.com>
2023-04-08 01:35:08 -0300

    Adicionado service

--- PraticandoComSpringBoot/src/main/java/io/github/arthursiq5/one/digitalinnovation/labpadroesdeprojeto/service/ClienteServiceImplementation.java
+++ PraticandoComSpringBoot/src/main/java/io/github/arthursiq5/one/digitalinnovation/labpadroesdeprojeto/service/ClienteServiceImplementation.java
@@ -5,6 +5,11 @@
 package io.github.arthursiq5.one.digitalinnovation.labpadroesdeprojeto.service;
 
 import io.github.arthursiq5.one.digitalinnovation.labpadroesdeprojeto.model.Cliente;
+import io.github.arthursiq5.one.digitalinnovation.labpadroesdeprojeto.model.ClienteRepository;
+import io.github.arthursiq5.one.digitalinnovation.labpadroesdeprojeto.model.Endereco;
+import io.github.arthursiq5.one.digitalinnovation.labpadroesdeprojeto.model.EnderecoRepository;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  *
@@ -12,29 +17,66 @@ import io.github.arthursiq5.one.digitalinnovation.labpadroesdeprojeto.model.Clie
  */
 public class ClienteServiceImplementation implements ClienteService {
 
+    @Autowired
+    private ClienteRepository clienteRepository;
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+    @Autowired
+    private ViaCepservice viaCepService;
+    
     @Override
     public Iterable<Cliente> buscarTodos() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        // Buscar todos os Clientes.
+        return clienteRepository.findAll();
     }
 
     @Override
     public Cliente buscarPorId(Long id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        // Buscar Cliente por ID.
+        Optional<Cliente> cliente = clienteRepository.findById(id);
+        return cliente.get();
     }
 
+    /**
+     * @param cliente cliente que quer adicionar
+     */
     @Override
     public void inserir(Cliente cliente) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        salvarClienteComCep(cliente);
     }
 
+    /**
+     * @param id ID do cliente que quer alterar
+     * @param cliente cliente (com os dados) que quer alterar
+     */
     @Override
     public void atualizar(Long id, Cliente cliente) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        Optional<Cliente> clienteBd = clienteRepository.findById(id);
+        if (clienteBd.isPresent()) {
+            salvarClienteComCep(cliente);
+        }
     }
 
+    /**
+     * @param id ID do cliente
+     */
     @Override
     public void deletar(Long id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        clienteRepository.deleteById(id);
+    }
+
+    private void salvarClienteComCep(Cliente cliente) {
+        // Verificar se o Endereco do Cliente já existe (pelo CEP).
+        String cep = cliente.getEndereco().getCep();
+        Endereco endereco = enderecoRepository.findById(cep).orElseGet(() -> {
+            // Caso não exista, integrar com o ViaCEP e persistir o retorno.
+            Endereco novoEndereco = viaCepService.consultarCep(cep);
+            enderecoRepository.save(novoEndereco);
+            return novoEndereco;
+        });
+        cliente.setEndereco(endereco);
+        // Inserir Cliente, vinculando o Endereco (novo ou existente).
+        clienteRepository.save(cliente);
     }
     
 }
